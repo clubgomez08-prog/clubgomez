b476f282ba470b11749b897cb758712e6ae29030
Cristian Alvarez <cristiantorres29052005@gmail.com>
2026-03-17 18:05:36 -0500

    feat: instalación píxel de Meta ID 825977093882247

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 // Nota: logo-rifex.png en /public es archivo legacy
 // Los logos activos son: logo_principal.png y logo_hero.png
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -20,6 +21,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '825977093882247');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=825977093882247&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
       <body
         className={`${poppins.variable} font-poppins antialiased`}
       >
